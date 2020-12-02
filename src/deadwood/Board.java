f822464b407c1d5fd04dc24160c4e3492fa9032b
Keegan-Mangahas <55812247+Keegan-Mangahas@users.noreply.github.com>
2020-12-01 18:05:35 -0800

    Getting rid of stuff

--- src/deadwood/Board.java
+++ src/deadwood/Board.java
@@ -14,33 +14,10 @@ public class Board extends Gamemaster{
     public Board() {
     }
 
-
-    // TODO: find out if this is necessary. Currently, Gamemaster has currentPlayer information, which Board
-    // should be able to call. Is Board an extension of Gamemaster?
-//    private Player getActivePlayer() {
-//        return null;
-//    }
-
-    // TODO: scrap?
-//    private Set[] getRoom(){
-//        return null;
-//    }
-
-    /**
-     * advanceDay
-     *
-     * Advances the current day.
-     */
     private void advanceDay() {
         currentDay++;
     }
 
-    /**
-     * wrapScene
-     *
-     * TODO: descriptive description for wrapScene
-     * @param scene scene object to wrap
-     */
     private void wrapScene(Scene scene){
         sceneCardsLeft--;
         /*
